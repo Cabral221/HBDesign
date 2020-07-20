c7f6d77f66f7b36f1bbeb600242149d9f61a01bc
Abdourahmane Diop <cabraldiop18@gmail.com>
2020-07-20 04:15:06 +0000

    fix css for secure asset heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @include('layouts.favicon')
         
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/style.css">
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>HB Design | Sénégal</title>
@@ -23,6 +23,6 @@
         <div class="footer bg-dark mb-0">
             @include('layouts.footer')
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
